7f235505da852fdd451e76339aef784566e0719d
Alexsansdraps <a.porhansl@icloud.com>
2018-05-18 16:54:30 +0200

    c

--- js/bouton1.js
+++ js/bouton1.js
@@ -4,3 +4,12 @@ $(function(){
       $('#hide').toggle() // AFFICHE ET CACHE A CHAQUE CLIQUE SUR LE BOUTTON
    });
 });
+
+function off() {
+document.getElementById("overlay").style.display="none";
+}
+
+
+function on() {
+document.getElementById("overlay").style.display="block";
+}
